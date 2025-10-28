32f459ec7c80934f7bf1a0c9849a2c10ad797e56
Ahmed Backer <ahmedbackerjr@gmail.com>
2025-10-28 09:30:31 +0000

    save asave and continue # Please enter the commit message for your changes. Lines starting

--- src/__tests__/Header.test.tsx
+++ src/__tests__/Header.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import Header from '../components/Header';
 import { Progress } from '../types';
 import { describe, it, expect } from 'vitest';
